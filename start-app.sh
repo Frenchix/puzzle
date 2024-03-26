586e059cbb6a79724ac25cccff93beb269cc5398
Frenchix <bapf62@hotmail.com>
2024-03-26 16:33:46 +0100

    modif fichier start-app

--- start-app.sh
+++ start-app.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 
 echo $serviceAccountKey > ./serviceAccountKey.json
+echo $fullchain > ./fullchain.pem
+echo $privkey > ./privkey.pem
 # Start default script for NodeJS apps
 npm start
